1f2b6254dbca8eb2b78e60a1ace94d93dbca42c2
Mohammad Aminifar <mohamadaminifar81@gmail.com>
2024-09-29 22:58:11 +0330

    admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.apps import apps
+#
+# models = apps.get_models()
+#
+# for model in models:
+#     try:
+#         admin.site.register(model)
+#     except admin.sites.AlreadyRegistered:
+#         pass
 
-# Register your models here.
+
+# from django.contrib import admin
+# from .models import CustomerProfile
+#
+#
+# class CustomerProfileAdmin(admin.ModelAdmin):
+#     is_admin = CustomerProfile.objects.all()
+#     list_display = ('name', 'email', 'phone')
+#     search_fields = ('name',)
+#     list_filter = ('name', 'email', 'phone')
+#
+#
+# admin.site.register(CustomerProfile, CustomerProfileAdmin)
