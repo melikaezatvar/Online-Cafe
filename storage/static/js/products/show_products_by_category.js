2f52c0edcf22db0d4debc2023839e1419b271a12
Ali-Ghorbanzadeh <biotick18311@gmail.com>
2024-09-30 13:24:56 +0330

    fix size default products image

--- storage/static/js/products/show_products_by_category.js
+++ storage/static/js/products/show_products_by_category.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div id="p-category-${category.name}" style="grid-template-columns: repeat(3, minmax(0, 1fr)); display: grid; gap: 50px"></div>`
                     category.get_products.forEach(product => {
                         menuEl.querySelector('#p-category-'+category.name).innerHTML += `
-                        <a href="/api/product/${product.id}" class="row product" style="">
+                        <a href="/api/product/${product.id}" class="row product" style="height: 25vh">
                             <div id="p-${product.id}" style="position: relative">
                                 <h5 class="menu-price">$${product.price}</h5>
                             </div>
@@ -18,11 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
                         if (product.images.length) {
                             menuEl.querySelector('#p-'+product.id).innerHTML += `
-                            <img class="rounded-circle mb-3 mb-sm-0 " src="${product.images[0].src}" alt="${product.images[0].alt}" style="max-width: 100%;
-    height: auto;">`
+                            <img class="rounded-circle mb-3 mb-sm-0" src="${product.images[0].src}" alt="${product.images[0].alt}" style="max-height: 20vh">`
                         } else {
                             menuEl.querySelector('#p-'+product.id).innerHTML += `
-                            <img class="rounded-circle mb-3 mb-sm-0" src="/media/product/images/site_image.jpeg" alt="no-image-available">`
+                            <img class="rounded-circle mb-3 mb-sm-0" src="/media/product/images/site_image.jpeg" alt="no-image-available" style="max-height: 20vh">`
                         }
                     })
                 }
