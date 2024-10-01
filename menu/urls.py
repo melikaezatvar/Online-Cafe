59773bcfebfe0b0ed3697321ce2496dc1143efe9
Ali-Ghorbanzadeh <biotick18311@gmail.com>
2024-10-01 15:14:32 +0330

    change favorite status

--- menu/urls.py
+++ menu/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('api/menu/', ProductAPIView.as_view(), name='product-list'),
     path('api/product/<int:pk>/', ProductAPIView.as_view(), name='product'),
     path('api/home/', HomeAPIView.as_view(), name='home'),
+    path('', HomeAPIView.as_view(), name='home'),
     path('api/category/', CategoryProductsAPIView.as_view(), name='category'),
     path('api/category/<str:name>/', CategoryAPIView.as_view(), name='category-name')
 ]
