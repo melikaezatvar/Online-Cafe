ded1aa80fde6766c9cb51301aaa8e906171aea41
Ali-Ghorbanzadeh <biotick18311@gmail.com>
2024-10-01 19:07:55 +0330

    update product details (previous description) and improve it for admin panel

--- menu/views.py
+++ menu/views.py
@@ -36,26 +36,26 @@ class ProductAPIView(APIView):
         return Response({'data': serializer.data}, status=status.HTTP_200_OK, template_name=template)
 
     # Create New Product (Requires admin access)
-    def post(self, request, *args, **kwargs):
-        serializer = ProductSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # Update Product (Requires admin access)
-    def put(self, request, *args, **kwargs):
-        product = Product.objects.filter(pk=kwargs['pk'])
-        serializer = ProductSerializer(product, data=request.data, partial=True, context={'request': request})
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # Delete Product (Requires admin access)
-    def delete(self, request, *args, **kwargs):
-        Product.objects.filter(pk=kwargs['pk']).update(is_delete=False)
-        return Response({"message": "Product deleted successfully"}, status=status.HTTP_200_OK)
+    # def post(self, request, *args, **kwargs):
+    #     serializer = ProductSerializer(data=request.data, context={'request': request})
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #
+    # # Update Product (Requires admin access)
+    # def put(self, request, *args, **kwargs):
+    #     product = Product.objects.filter(pk=kwargs['pk'])
+    #     serializer = ProductSerializer(product, data=request.data, partial=True, context={'request': request})
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #
+    # # Delete Product (Requires admin access)
+    # def delete(self, request, *args, **kwargs):
+    #     Product.objects.filter(pk=kwargs['pk']).update(is_delete=False)
+    #     return Response({"message": "Product deleted successfully"}, status=status.HTTP_200_OK)
 
 
 class CategoryProductsAPIView(ListAPIView):
