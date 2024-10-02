ad0251222367457aaba7ac9d2e2fd964d14b82a0
Mohammad Aminifar <mohamadaminifar81@gmail.com>
2024-10-02 15:08:12 +0330

    admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
+# from django.contrib import admin
+# from django.apps import apps
+#
+# models = apps.get_models()
+#
+# for model in models:
+#     try:
+#         admin.site.register(model)
+#     except admin.sites.AlreadyRegistered:
+#         pass
 from django.contrib import admin
+from .models import CustomerProfile
 
-# Register your models here.
+
+class CustomerProfileAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone_number', 'is_ban', 'is_staff')
+    list_filter = ('email', 'phone_number')
+    actions = ['make_staff', 'ban_customer']
+
+    def make_staff(self, request, queryset):
+        queryset.update(is_staff=True)
+        self.message_user(request, "Selected customers have been granted staff access.")
+
+    make_staff.short_description = "Grant staff access to selected customers"
+
+    def ban_customer(self, request, queryset):
+        queryset.update(is_ban=True)
+        self.message_user(request, "Selected customers have been banned.")
+
+    ban_customer.short_description = "Ban selected customers"
+
+
+admin.site.register(CustomerProfile, CustomerProfileAdmin)
