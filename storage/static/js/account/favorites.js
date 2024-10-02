c502a38396147ac215563b610594bb0a756dc79f
melikaezatvar <melikaezatvar@gmail.com>
2024-10-02 17:21:50 +0330

    Styling better favorite list in user profile.

--- storage/static/js/account/favorites.js
+++ storage/static/js/account/favorites.js
@@ -5,20 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch('/api/favorites/')
             .then(response => response.json())
             .then(data => {
-                favoriteList.innerHTML = '';
-                data.forEach(product => {
-                    const li = document.createElement('li');
-                    li.innerHTML = `${product.name} - Price: $${product.price} 
-                                    <a href="#" class="remove-favorite-btn" data-id="${product.id}">Delete</a>`;
-                    favoriteList.appendChild(li);
-                });
+                favoriteList.innerHTML = ''
+                if (data.length === 0) {
+                    favoriteList.innerHTML = '<li>No items in favorite list.</li>';
+                } else {
+                    data.forEach(product => {
+                        const li = document.createElement('li');
+                        li.style.position = 'relative';
+
+                        const imageSrc = product.images.length ? product.images[0].src : '/media/product/images/site_image.jpeg'; // Path to your default image
+                        li.innerHTML += `
+                            <div class="row product" style="display: flex; align-items: center; padding: 10px; border: 1px solid #ddd; border-radius: 10px;">
+                                <img src="${imageSrc}" class="rounded-circle mb-3 mb-sm-0" style="width: 100px; height: 100px; object-fit: cover; margin-right: 10px;">
+                                <div style="display: flex; flex-direction: column;">
+                                    <div style="font-size: 20px; font-weight: bold;">$${product.price}</div>
+                                    <div style="font-size: 18px;">${product.name}</div>
+                                </div>
+                            </div>
+                            <a href="#" class="remove-favorite-btn" data-id="${product.id}" style="position: absolute; top: 10px; right: 10px; background: none; border: none; cursor: pointer;">
+                                <img src="/static/img/red-delete-10437.png" alt="Delete" style="width: 20px; height: 20px;">
+                            </a>
+                        `;
+
+                        favoriteList.appendChild(li);
+                    });
+                }
             })
             .catch(err => console.error('Error fetching favorites:', err));
     };
 
-
     const handleRemoveFavorite = (event) => {
-        const productId = event.target.getAttribute('data-id');
+        const productId = event.target.closest('.remove-favorite-btn').getAttribute('data-id');
 
         fetch(`/api/favorites/${productId}/`, {
             method: 'POST',
@@ -37,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     favoriteList.addEventListener('click', (event) => {
-        if (event.target.classList.contains('remove-favorite-btn')) {
+        if (event.target.closest('.remove-favorite-btn')) {
             handleRemoveFavorite(event);
         }
     });
