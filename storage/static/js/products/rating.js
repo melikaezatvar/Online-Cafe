45622ee2d78226f24540d07cae01bcfe363ab793
Ali-Ghorbanzadeh <biotick18311@gmail.com>
2024-10-03 15:46:04 +0330

    add rate for products

--- storage/static/js/products/rating.js
+++ storage/static/js/products/rating.js
@@ -1,12 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (qualifiedName, value) => {
+
+    const productEl = document.querySelector('#product-id')
+
+    fetch(`/api/product/rate/${productEl.value}/`)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('#star' + data[0].rate).setAttribute('checked', value)
+        }).catch(() => {})
+
     document.querySelectorAll('.star-rating input').forEach((rate) => {
         rate.addEventListener('change',  () => {
-            fetch(`/api/rating/${this.value}`, {
+            fetch(`/api/product/rate/${productEl.value}/${rate.value}/`, {
                 method: 'POST',
                 headers:{
                     'X-CSRFToken': document.querySelector(`input[name="csrfmiddlewaretoken"]`).value,
                 },
-            })
+            }).then(res => console.log(res))
         });
     });
 })
