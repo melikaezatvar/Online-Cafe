ba0306b6350606764bb70d57890a27f429e0f0b1
Ali-Ghorbanzadeh <biotick18311@gmail.com>
2024-10-03 17:05:52 +0330

    some bug fixed

--- storage/static/js/account/login.js
+++ storage/static/js/account/login.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     loginForm.addEventListener('click', (event) => {
         const data = new FormData(document.querySelector('#login-form'));
-        console.log(data)
         fetch('/api/login/', {
             method: 'POST',
             body: data,
