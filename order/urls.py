fbad5d7f7bacce2c79078f67cd78be4094e158af
Akbari-J <akbari1582@gmail.com>
2024-10-04 12:19:12 +0330

    order details in profile by click on orders

--- order/urls.py
+++ order/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from .views import UserOrderListCreateView, OrderUpdateView, CancelOrderView, OrderItemUpdateDeleteView
-from .views import AddToCartAPIView, UserOrderListView, RemoveOrderAPIView
+from .views import AddToCartAPIView, UserOrderListView, RemoveOrderAPIView, OrderDetailAPIView
 
 urlpatterns = [
     path('api/order/add-to-cart/', AddToCartAPIView.as_view(), name='add_to_cart'),
     path('api/orders/', UserOrderListView.as_view(), name='user-order-list'),
     path('order/remove/<int:order_id>/', RemoveOrderAPIView.as_view(), name='remove-order'),
+    path('api/order/<int:order_id>/', OrderDetailAPIView.as_view(), name='order-detail'),
 
 ]
--- order/views.py
+++ order/views.py
@@ -9,6 +9,8 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
 
+
+
 class AddToCartAPIView(GenericAPIView, ListModelMixin):
     serializer_class = OrderSerializer
     # print(10)
@@ -64,110 +66,12 @@ class RemoveOrderAPIView(APIView):
 
 
 
-# # لیست سفارشات و ایجاد سفارش جدید هنگام افزودن کالا به سبد خرید
-# class UserOrderListCreateView(ListCreateAPIView):
-#     serializer_class = OrderSerializer
-#     permission_classes = [IsAuthenticated]
-#
-#     def get_queryset(self):
-#         print("123")
-#         return Order.objects.filter(user=self.request.user)
-#
-#     def post(self, request, *args, **kwargs):
-#         product_id = request.data.get('product_id')
-#         product = Product.objects.get(id=product_id)
-#
-#
-#         # ایجاد یا پیدا کردن سفارش با وضعیت pending
-#         order, created = Order.objects.get_or_create(user=request.user, status='pending')
-#
-#         print(product,'2')
-#
-#         # ایجاد یا پیدا کردن آیتم سفارش
-#         order_item, item_created = OrderItem.objects.get_or_create(order=order, product=product)
-#         print(product, '22')
-#         if not item_created:
-#             order_item.quantity += 1
-#             print(product, 3)
-#         else:
-#             order_item.quantity = 1
-#             print(product, 4)
-#         order_item.save()
-#
-#         print(product, 5)
-#         serializer = self.get_serializer(order)
-#         print(product, 6)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#
-#
-# # به‌روزرسانی وضعیت سفارش
-# class OrderUpdateView(RetrieveUpdateAPIView):
-#     serializer_class = OrderSerializer
-#     permission_classes = [IsAuthenticated]
-#
-#     def get_queryset(self):
-#         # فقط سفارش‌های pending قابل ویرایش هستند - یا بهتره کل سفارش ها قابل ویرایش باشه
-#         return Order.objects.filter(user=self.request.user, status='pending')
-#
-#     def update(self, request, *args, **kwargs):
-#         order = self.get_object()
-#         new_status = request.data.get('status')
-#
-#         # بررسی وضعیت جدید سفارش
-#         if new_status not in ['shipped', 'delivered', 'cancelled']:
-#             return Response({'error': 'Invalid status'}, status=status.HTTP_400_BAD_REQUEST)
-#
-#         # به‌روزرسانی وضعیت سفارش
-#         order.status = new_status
-#         order.save()
-#
-#         serializer = self.get_serializer(order)
-#         return Response(serializer.data)
-#
-#
-# # لغو سفارش
-# class CancelOrderView(APIView):
-#     permission_classes = [IsAuthenticated]
-#
-#     def post(self, request, order_id):
-#         order = Order.objects.filter(id=order_id, user=request.user).first()
-#
-#         if not order:
-#             return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
-#
-#         if order.status == 'pending':
-#             order.status = 'cancelled'
-#             order.save()
-#             return Response({'message': 'Order cancelled successfully'}, status=status.HTTP_200_OK)
-#         else:
-#             return Response({'error': 'Cannot cancel this order'}, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-#
-#
-# # ویرایش و حذف آیتم سفارش
-# class OrderItemUpdateDeleteView(RetrieveUpdateDestroyAPIView):
-#     serializer_class = OrderItemUpdateSerializer
-#     permission_classes = [IsAuthenticated]
-#
-#     def get_queryset(self):
-#         # فقط آیتم‌های مربوط به سفارشات کاربر
-#         return OrderItem.objects.filter(order__user=self.request.user)
-#
-#     def update(self, request, *args, **kwargs):
-#         order_item = self.get_object()
-#         serializer = self.get_serializer(order_item, data=request.data, partial=True)
-#         serializer.is_valid(raise_exception=True)
-#
-#         # به‌روزرسانی تعداد
-#         order_item.quantity = serializer.validated_data['quantity']
-#         order_item.save()
-#
-#         return Response({'message': 'Order item updated successfully.'}, status=status.HTTP_200_OK)
-#
-#     def destroy(self, request, *args, **kwargs):
-#         order_item = self.get_object()
-#
-#         # حذف آیتم از سفارش
-#         order_item.delete()
-#         return Response({'message': 'Order item deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
+
+class OrderDetailAPIView(APIView):
+    def get(self, request, order_id, *args, **kwargs):
+        try:
+            order = Order.objects.get(id=order_id, user=request.user)
+            serializer = OrderSerializer(order)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Order.DoesNotExist:
+            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
