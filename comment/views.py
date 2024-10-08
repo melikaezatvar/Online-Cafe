ecbc279a54ca070f084b6778cdf51b12db64e6b5
Ali-Ghorbanzadeh <biotick18311@gmail.com>
2024-10-09 02:17:09 +0330

    add comment app

---
+++ comment/views.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Comment
+from .serializer import CommentSerializer
+
+
+class CommentManagementAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, product_id, *args, **kwargs):
+        comments = Comment.objects.filter(product_id=product_id, is_active=True).order_by('-create_at')[:20]
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, product_id, *args, **kwargs):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, product_id=product_id)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, comment_id, *args, **kwargs):
+        comment = Comment.objects.filter(id=comment_id)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
